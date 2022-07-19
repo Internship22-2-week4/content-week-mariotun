ff56989c9bf2aa2355350bfdc656f1b3f44bca8b
mariotun <oriamnut09@gmail.com>
2022-07-19 10:43:48 -0600

    Agregando las vistas de autores y libros

--- day_4/book-app/books/urls.py
+++ day_4/book-app/books/urls.py
@@ -8,15 +8,17 @@ from rest_framework.routers import DefaultRouter
 
 #views
 #from . import views
-from .views import CategoryViewSet
+from .views import CategoryViewSet,AuthorViewSet,BookViewSet
 
 router = DefaultRouter()
 router.register(r'categories',CategoryViewSet)
+router.register(r'author',AuthorViewSet)
+router.register(r'book',BookViewSet)
 
 urlpatterns = router.urls
 
 urlpatterns += [
-    
+
     #path('',views.index, name='index'),
     #path('author/<int:author_id>',views.author, name='author'),
     #path('category/<int:category_id>',views.category, name='category')
--- day_4/book-app/books/views.py
+++ day_4/book-app/books/views.py
@@ -16,7 +16,13 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 """
 def index(request):
